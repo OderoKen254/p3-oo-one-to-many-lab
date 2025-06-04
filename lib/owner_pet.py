693b6fed4e3c3d645dafeb55b70cfdb47808ec40
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-06-04 23:39:46 +0300

    add pet class and update all the instances of the pet class

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,40 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if not isinstance(name, str):
+            raise Exception("Name must be a string")
+        if not name:
+            raise Exception("Name cannot be empty")
+        if pet_type not in self.PET_TYPES:
+            raise Exception("pet_type must be one of: " + ", ".join(self.PET_TYPES))
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("owner must be an instance of Owner")
+        
+        self._name = name
+        self._pet_type = pet_type
+        self._owner = owner
+        Pet.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @property
+    def owner(self):
+        return self._owner
+    
+    @owner.setter
+    def owner(self, value):
+        if value is not None and not isinstance(value, Owner):
+            raise Exception("owner must be an instance of Owner")
+        self._owner = value
+        
 
 class Owner:
     def __init__(self, name):
